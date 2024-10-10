ed1d1bf53f23856598b886beeaffccfc32e81c9d
Paul Irem <chinonsopaul@gmail.com>
2024-10-10 21:44:30 +0100

    chore

--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -1,7 +1,49 @@
 import * as React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import { useFormik } from "formik";
+import { useSelector } from "react-redux";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import validationSchema from "../../../../shared/login.schema";
+//use redux to manage state
 
-const Login:React.FC = function () {
-  return <div>Login</div>;
-}
+const Login: React.FC = function () {
+  const model = useSelector((data) => data.user);
+  const controller = useController();
+  const { values, isValid, dirty } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: toFormikValidationSchema(validationSchema),
+    onSubmit: (values) => controller.login(values),
+  });
+  return (
+    <Box component="form" aria-label="Login" noValidate onSubmit={handleSubmit}>
+      <TextField
+        placeholder="Email"
+        type="email"
+        name="email"
+        variant="outlined"
+        aria-label="email"
+        value={values.email}
+      />
+      <TextField
+        placeholder="Password"
+        value={values.password}
+        name="password"
+        type="password"
+        aria-label="password"
+        variant="outlined"
+      />
+      <Button
+        disabled={(!isValid && dirty) || !dirty}
+        type="submit"
+        aria-labe="submit"
+      >
+        Submit
+      </Button>
+    </Box>
+  );
+};
 
 export default Login;
