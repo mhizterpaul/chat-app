80e6391c9a2eb4144ca970505b9f2e59aa666377
Paul Irem <dev.paulirem@gmail.com>
2025-06-13 18:43:20 +0100

    fix: button text div  selector

--- client/src/pages/newChat.tsx
+++ client/src/pages/newChat.tsx
@@ -134,7 +134,7 @@ function NewConversation() {
                 textTransform: "capitalize",
                 fontWeight: "bold",
               },
-              "& div.css-18hvpog-MuiPaper-root-MuiSnackbarContent-root": {
+              "& button.Mui-selected > div.MuiSnackbarContent-root": {
                 boxShadow: "0px 1px 5px -1px rgba(0,0,0,0.2), 0px 2px 10px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12)",
               },
               "& button.Mui-selected": {
